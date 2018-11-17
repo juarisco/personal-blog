2c3c0b57cac1590386d62e704a6098f615b8690f
juarisco <juarisco@outlook.com>
2018-11-17 15:36:04 -0500

    Creating publicController, about, post and contact pages

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,10 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PublicController@index');
+Route::get('post/{id}', 'PublicController@singlePost');
+Route::get('about', 'PublicController@about');
+Route::get('contact', 'PublicController@contact');
+Route::post('contact', 'PublicController@contactPost');
