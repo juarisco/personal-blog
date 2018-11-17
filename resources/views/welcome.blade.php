e0c49e1d05e979dcf638b8c8074f27274b31b1fe
juarisco <juarisco@outlook.com>
2018-11-17 16:38:29 -0500

    Building urls for assets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     
     <!-- Page Header -->
-    <header class="masthead" style="background-image: url('assets/img/home-bg.jpg')">
+    <header class="masthead" style="background-image: url('{{ asset('assets/img/home-bg.jpg') }}')">
     <div class="overlay"></div>
     <div class="container">
         <div class="row">
