e8e300aa2bad8ab924c82ac647f92e230a88d5b5
juarisco <juarisco@outlook.com>
2018-11-18 15:48:01 -0500

    Protecting admin panel with middleware CheckRole

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('checkRole:admin');
+    }
+
     public function dashboard()
     {
         return view('admin.dashboard');
