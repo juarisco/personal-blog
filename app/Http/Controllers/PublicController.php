711374702c0f2f87f90e3b5edcac66ca06e7394b
juarisco <juarisco@outlook.com>
2018-11-19 17:05:44 -0500

    Displaying posts and comments (Relationships)

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PublicController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $posts = Post::all();
+
+        return view('welcome', compact('posts'));
     }
 
     public function singlePost($id)
