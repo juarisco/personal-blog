42bb09b8d8ef70ae9ac8ece83cec8b4d0132667d
juarisco <juarisco@outlook.com>
2018-11-20 11:45:25 -0500

    Single post and comments

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -14,9 +14,10 @@ class PublicController extends Controller
         return view('welcome', compact('posts'));
     }
 
-    public function singlePost($id)
+    public function singlePost(Post $post)
     {
-        return view('singlePost');
+
+        return view('singlePost', compact('post'));
     }
 
     public function about()
