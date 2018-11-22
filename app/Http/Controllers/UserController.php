f3926fd532df54b89ebaaac8ca23653adf0166f2
juarisco <juarisco@outlook.com>
2018-11-22 16:11:04 -0500

    Password change

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Comment;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserUpdate;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -39,6 +40,26 @@ class UserController extends Controller
         $user->name = $request->name;
         $user->email = $request->email;
         $user->save();
+
+        if ($request->password) {
+            if (!(Hash::check($request->password, Auth::user()->password))) {
+                return redirect()->back()->with('error', "Your current password does not match with the password you provided");
+            }
+
+            if (strcmp($request->password, $request->new_paaword) == 0) {
+                return redirect()->back()->with('error', "New password cannot be some as your current password");
+            }
+
+            $validation = $request->validate([
+                'password' => 'required',
+                'new_password' => 'required|string|min:6|confirmed'
+            ]);
+
+            $user->password = bcrypt($request->new_password);
+            $user->save();
+
+            return redirect()->back()->with('success', "Password changed successfully");
+        }
 
         return back();
     }
